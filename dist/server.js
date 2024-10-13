6ad92e9fafc81b7aa3831fec3c395b4f49837ccf
Olena-Rudnieva <rudnievao@gmail.com>
2024-10-14 02:27:19 +0300

    Added first files

--- dist/server.js
+++ dist/server.js
@@ -1,2 +1,13 @@
-console.log('hi');
-export {};
+// import mongoose from 'mongoose';
+import app from './app.js';
+const { PORT = 3002 } = process.env;
+app.listen(PORT, () => console.log(`Server running at ${PORT} port`));
+// mongoose
+//   .connect(DB_HOST as string)
+//   .then(() => {
+//     app.listen(PORT, () => console.log(`Server running at ${PORT} port`));
+//   })
+//   .catch((error: any) => {
+//     console.log(error.message);
+//     process.exit(1);
+//   });
