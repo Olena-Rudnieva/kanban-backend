07e0672d27b6f599e284d34e711ea7f13ca8cd3a
Olena-Rudnieva <rudnievao@gmail.com>
2024-10-18 12:22:33 +0300

    Final fixes

--- eslint.config.js
+++ eslint.config.js
@@ -1,34 +1,3 @@
-// import globals from 'globals';
-// import pluginJs from '@eslint/js';
-// import tseslint from '@typescript-eslint/eslint-plugin';
-// import pluginReact from 'eslint-plugin-react';
-
-// export default [
-//   {
-//     files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
-//     languageOptions: {
-//       globals: globals.browser,
-//       parser: '@typescript-eslint/parser',
-//       parserOptions: {
-//         ecmaVersion: 2021,
-//         sourceType: 'module',
-//       },
-//     },
-//     rules: {
-//       'no-unused-vars': 'warn',
-//       semi: ['error', 'always'],
-//       quotes: ['error', 'single'],
-//       '@typescript-eslint/no-explicit-any': 'off',
-//       // ...tseslint.configs.recommended.rules,
-//     },
-//   },
-//   {
-//     ignores: ['dist/**'],
-//   },
-//   pluginJs.configs.recommended,
-//   pluginReact.configs.flat.recommended,
-// ];
-
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
